a35c6bd44b3ea1ed732d69e7f1ee3d8f715020e7
Sufyan Liaquat <sufyanliaquat58@gmail.com>
2025-12-14 18:55:03 +0500

    feat: add Redis configuration and connection handling

--- Server/src/lib/redis.js
+++ Server/src/lib/redis.js
@@ -2,9 +2,19 @@ import Redis from 'ioredis';
 import { ENV } from '../util/dotenv.js';
 
 const redisConnection = new Redis(ENV.REDIS_URL, {
-    maxRetriesPerRequest: null
+    maxRetriesPerRequest: null,
+    retryStrategy(times) {
+        const delay = Math.min(times * 50, 2000);
+        return delay;
+    },
+    enableReadyCheck: true,
+    lazyConnect: false
 });
 
-redisConnection.on('error', (err) => console.log('Redis Client Error', err));
+redisConnection.on('error', (err) => {
+    console.log('Redis Client Error', err);
+});
 
-export { redisConnection };
+redisConnection.on('connect', () => {
+    console.log('Redis connected successfully');
+});
